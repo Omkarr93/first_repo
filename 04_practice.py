3e9652a34cd6bacc7210acf0a97d4104eb12af2c
Omkarr93 <90467263+Omkarr93@users.noreply.github.com>
2021-10-05 10:50:23 +0530

    Add files via upload

---
+++ 04_practice.py
@@ -0,0 +1,5 @@
+a= int(input("Enter 1st number\n"))
+b= int(input("Enter 2nd number\n"))
+avg= (a+b)/2
+print(avg)
+
---
+++ if_else_02.py
@@ -0,0 +1,7 @@
+L =  int(input('Length of square'))
+b = int(input('Breadth of square'))
+
+if L == b :
+    print('It is square')
+else:
+    print('NO square')    
